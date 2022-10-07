a33667366fdf261ec542b4f19d2d4085a12d9dd2
nicholascarlin <54487246+nicholascarlin@users.noreply.github.com>
2022-10-07 15:31:26 -0500

    Better

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import Header from './components/UI/Header';
 import React from 'react';
-import { getNoteFromInterval } from './utils/Intervals';
 
 function App() {
 	return (
-		<div className='w-screen h-screen p-4'>
+		<div className='w-screen h-screen'>
 			<Header />
-			<div>test2</div>
-			<button
-				onClick={() => {
-					getNoteFromInterval('A', 2);
-				}}>
-				test
-			</button>
+			<div className='w-full h-full bg-red-500'></div>
 		</div>
 	);
 }
--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,27 +1,94 @@
-import { AiFillCaretLeft } from 'react-icons/ai';
-import React from 'react';
+import './Header.css';
 
-const Header = () => {
-	let headerItem = 'bg-gray-300 p-4 rounded-xl inline-flex items-center';
-	let headerIcon = 'ml-2';
+import { createRef, useEffect, useRef, useState } from 'react';
+
+import gsap from 'gsap';
+
+const items = [
+	{
+		name: 'Intervals',
+		color: '#f44336',
+		href: '#',
+	},
+	{
+		name: 'Fretboard',
+		color: '#e91e63',
+		href: '#',
+	},
+	{
+		name: 'Scales',
+		color: '#9c27b0',
+		href: '#',
+	},
+	{
+		name: 'Chords',
+		color: '#673ab7',
+		href: '#',
+	},
+	{
+		name: 'Ear',
+		color: '#3f51b5',
+		href: '#',
+	},
+];
+
+const Header = ({}) => {
+	const $root = useRef();
+	const $indicator1 = useRef();
+	const $indicator2 = useRef();
+	const $items = useRef(items.map(createRef));
+	const [active, setActive] = useState(0);
+
+	const animate = () => {
+		const menuOffset = $root.current.getBoundingClientRect();
+		const activeItem = $items.current[active].current;
+		const { width, height, top, left } = activeItem.getBoundingClientRect();
+
+		const settings = {
+			x: left - menuOffset.x,
+			y: top - menuOffset.y,
+			width: width,
+			height: height,
+			backgroundColor: items[active].color,
+			ease: 'elastic.out(.7, .7)',
+			duration: 0.8,
+		};
+
+		gsap.to($indicator1.current, {
+			...settings,
+		});
+
+		gsap.to($indicator2.current, {
+			...settings,
+			duration: 1,
+		});
+	};
+
+	useEffect(() => {
+		animate();
+		window.addEventListener('resize', animate);
+
+		return () => {
+			window.removeEventListener('resize', animate);
+		};
+	}, [active]);
 
 	return (
-		<div className='container mx-auto bg-gray-200 rounded-xl shadow border p-8 fr-center-around-full'>
-			<div className={headerItem}>
-				Intervals <AiFillCaretLeft className={headerIcon} />
-			</div>
-			<div className={headerItem}>
-				Fretboard <AiFillCaretLeft className={headerIcon} />
-			</div>
-			<div className={headerItem}>
-				Scales <AiFillCaretLeft className={headerIcon} />
-			</div>
-			<div className={headerItem}>
-				Chords <AiFillCaretLeft className={headerIcon} />
-			</div>
-			<div className={headerItem}>
-				Ear <AiFillCaretLeft className={headerIcon} />
-			</div>
+		<div ref={$root} className='menu'>
+			{items.map((item, index) => (
+				<a
+					key={item.name}
+					ref={$items.current[index]}
+					className={`item ${active === index ? 'active' : ''}`}
+					onMouseEnter={() => {
+						setActive(index);
+					}}
+					href={item.href}>
+					{item.name}
+				</a>
+			))}
+			<div ref={$indicator1} className='indicator' />
+			<div ref={$indicator2} className='indicator' />
 		</div>
 	);
 };
