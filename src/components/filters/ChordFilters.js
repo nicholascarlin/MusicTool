9fc11af8027720b5a20f94dbe31f64f33ccc6d93
nicholascarlin <54487246+nicholascarlin@users.noreply.github.com>
2022-10-21 12:10:57 -0500

    Fixed Chord Filter Showing Wrong Info

--- src/components/filters/ChordFilters.js
+++ src/components/filters/ChordFilters.js
@@ -8,21 +8,7 @@ const ChordFilters = ({ SetActiveSubTask }) => {
 	return (
 		<>
 			<div className='-mt-14 mb-8 underline text-lg'>Chord Filters</div>
-			<div
-				onClick={() => {
-					inputRef.current.focus();
-				}}
-				className='relative w-3/4 border-2 p-2 rounded-xl'>
-				<AiFillCaretDown className='absolute right-2 top-0 bottom-0 my-auto' />
-				<select
-					onChange={(e) => SetActiveSubTask(e.target.value)}
-					ref={inputRef}
-					className='appearance-none w-full'
-					placeholder='Choose Your Interval Excercise'>
-					<option value={0}>Find Interval From Two Notes</option>
-					<option value={1}>Find Note From Interval</option>
-				</select>
-			</div>
+			<div className='mt-4'>COMING NEXT</div>
 		</>
 	);
 };
